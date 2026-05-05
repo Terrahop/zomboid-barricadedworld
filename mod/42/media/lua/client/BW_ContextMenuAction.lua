25d2299e3a084f5d246d95ef3c1fadcc068a8438
terrahop <alex@terrahop.dev>
2026-05-05 13:30:05 +0200

    feat: improve context menu options for erosion protection

--- mod/42/media/lua/client/BW_ContextMenuAction.lua
+++ mod/42/media/lua/client/BW_ContextMenuAction.lua
@@ -1,30 +1,48 @@
-if BarricadedWorld == nil then BarricadedWorld = {} end
+if BarricadedWorld == nil then
+  BarricadedWorld = {}
+end
 
 function BarricadedWorld.setProtection(isoObject, value)
-    local modData = isoObject:getModData()
-    modData["BarricadedWorld:isPlayerPlaced"] = value
-    isoObject:transmitModData()
+  local modData = isoObject:getModData()
+  modData["BarricadedWorld:isPlayerPlaced"] = value
+  isoObject:transmitModData()
 end
 
-function BarricadedWorld.contextMenuOptions(player, context, worldobjects)
-    local playerObj = getSpecificPlayer(player)
-    local tileIsoObject = nil
-    for _, v in ipairs(worldobjects) do
-        if instanceof(v, "IsoDoor") then
-            tileIsoObject = v;
-        elseif instanceof(v, "IsoWindow") then
-            tileIsoObject = v;
-        end
+---@param playerIndex integer
+---@param context ISContextMenu
+---@param worldobjects IsoObject[]
+---@diagnostic disable-next-line: unused-local
+function BarricadedWorld.contextMenuOptions(playerIndex, context, worldobjects)
+  -- local playerObj = getSpecificPlayer(playerIndex)
+  local tileIsoObject = nil
+
+  for _, worldObject in ipairs(worldobjects) do
+    if instanceof(worldObject, "IsoDoor") then
+      tileIsoObject = worldObject
+    elseif instanceof(worldObject, "IsoWindow") then
+      tileIsoObject = worldObject
     end
-    -- Protect from erosion
-    if tileIsoObject then
-        local modData = tileIsoObject:getModData()
-        if modData["BarricadedWorld:isPlayerPlaced"] then
-            context:addOption("Remove erosion protection", tileIsoObject, BarricadedWorld.setProtection, false)
-        else
-            context:addOption("Protect from erosion", tileIsoObject, BarricadedWorld.setProtection, true)
-        end
+  end
+
+  if tileIsoObject then
+    local modData = tileIsoObject:getModData()
+
+    if modData["BarricadedWorld:isPlayerPlaced"] then
+      context:addOption(
+        "[BarricadedWorld] Disable erosion protection for " .. tileIsoObject:getName(),
+        tileIsoObject,
+        BarricadedWorld.setProtection,
+        false
+      )
+    else
+      context:addOption(
+        "[BarricadedWorld] Enable erosion protection for " .. tileIsoObject:getName(),
+        tileIsoObject,
+        BarricadedWorld.setProtection,
+        true
+      )
     end
+  end
 end
 
-Events.OnFillWorldObjectContextMenu.Add(BarricadedWorld.contextMenuOptions);
+Events.OnFillWorldObjectContextMenu.Add(BarricadedWorld.contextMenuOptions)
